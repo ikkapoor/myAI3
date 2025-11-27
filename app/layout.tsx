812c43163bba14e71d363c5ef898eedbde9bc284
ikkapoor <53040363+ikkapoor@users.noreply.github.com>
2025-11-27 19:18:36 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyAI3",
-  description: "MyAI3",
+  title: "Nitibot",
+  description: "Nitibot",
 };
 
 export default function RootLayout({
